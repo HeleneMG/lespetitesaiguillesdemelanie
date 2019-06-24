1610a4cb8884b72123827348f654e10f2c388f91
HeleneMG <helenemouttetg@gmail.com>
2019-06-24 14:12:30 +0200

    comite

---
+++ script.js
@@ -0,0 +1,23 @@
+ M.AutoInit();
+
+//SCROLL REVEAL
+ScrollReveal().reveal('h1', {
+    duration: 3000,
+    scale: 0.8,
+    reset: true
+});
+
+ScrollReveal().reveal('h2', {
+    duration: 4000,
+    scale: 0.6,
+    reset: true
+});
+
+ScrollReveal().reveal('.ser', {
+    duration: 1500,
+    distance: '200px',
+    origin: 'left',
+    //scale: 1,
+    reset: true
+});
+
