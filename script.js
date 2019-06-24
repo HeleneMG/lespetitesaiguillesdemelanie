718d62dad7e446aef61c1f555622eb8ff8bdda83
HeleneMG <helenemouttetg@gmail.com>
2019-06-24 15:02:26 +0200

    js OK

--- script.js
+++ script.js
@@ -14,17 +14,46 @@ ScrollReveal().reveal('h2', {
     reset: true
 });
 
-ScrollReveal().reveal('.ser', {
+ScrollReveal().reveal('.ser1', {
+    duration: 3500,
+    distance: '200px',
+    origin: 'left',
+    //scale: 1,
+    reset: true
+});
+ScrollReveal().reveal('.ser2', {
+    duration: 2500,
+    distance: '200px',
+    origin: 'left',
+    //scale: 1,
+    reset: true
+});
+ScrollReveal().reveal('.ser3', {
     duration: 1500,
     distance: '200px',
     origin: 'left',
     //scale: 1,
     reset: true
 });
+
 ScrollReveal().reveal('.robe', {
     duration: 1500,
     distance: '200px',
     origin: 'left',
+    //scale: 1,
+    reset: true
+});
+ScrollReveal().reveal('.AA1', {
+    duration: 2500,
+    distance: '200px',
+    origin: 'left',
+    //scale: 1,
+    reset: true
+});
+ScrollReveal().reveal('.AA2', {
+    duration: 2000,
+    distance: '200px',
+    origin: 'right',
     //scale: 1,
     reset: true
 });
